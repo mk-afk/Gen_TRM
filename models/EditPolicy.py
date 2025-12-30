db5a19d98bc760314a140243cf98c29097e87833
mk-afk <mkireeff@gmail.com>
2025-12-29 19:52:43 -0800

    Update EditPolicy.py

--- models/EditPolicy.py
+++ models/EditPolicy.py
@@ -16,24 +16,27 @@ class EditPolicy(nn.Module):
         if tokens.dim() == 1:
             tokens = tokens.unsqueeze(0)
 
-        h = self.trm(tokens, return_hidden=True)
-        h_last = h[:, -1, :]
+        # LanguageTRM ALWAYS returns a dict
+        out = self.trm(tokens)
+        h = out["hidden"]                 # (B, T, D)
+        h_last = h[:, -1, :]              # (B, D)
 
         action_logits = self.action_head(h_last)
         token_logits  = self.token_head(h_last)
 
-        out = {
+        result = {
             "action_logits": action_logits,
             "token_logits": token_logits,
         }
 
         if return_hidden:
-            out["hidden_states"] = h_last
+            result["hidden_states"] = h_last
 
-        # unbatch
+        # Unbatch for convenience
         if action_logits.shape[0] == 1:
-            for k in out:
-                out[k] = out[k].squeeze(0)
+            for k in result:
+                result[k] = result[k].squeeze(0)
 
-        return out
+        return result
+
 
